bf626e25a10c95ce2643e1ebe855dbefca03af35
Vladislav Stognienko <stognienkovv@gmail.com>
2018-03-12 16:07:04 +0200

    XML parser

--- SentDex/AcessingTheInternet.go
+++ SentDex/AcessingTheInternet.go
@@ -3,38 +3,49 @@ package main
 import (
 	"fmt"
 	"encoding/xml"
+	"net/http"
+	"io/ioutil"
 )
 
-var washPostXML = []byte(`
-<sitemapindex>
-   <sitemap>
-      <loc>http://www.washingtonpost.com/news-politics-sitemap.xml</loc>
-   </sitemap>
-   <sitemap>
-      <loc>http://www.washingtonpost.com/news-blogs-politics-sitemap.xml</loc>
-   </sitemap>
-   <sitemap>
-      <loc>http://www.washingtonpost.com/news-opinions-sitemap.xml</loc>
-   </sitemap>
-</sitemapindex>
-`)
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+	}
 
-type Location struct{
-	Loc string `xml:"loc"`
+type NewsMap struct {
+	Keyword string
+	Location string
 }
 
 
 type Sitemapindex struct{
-	Locations []Location `xml:"sitemap"`
+	Locations []string `xml:"sitemap>loc"`
 }
 
-func (e Location) String () string{
-	return fmt.Sprint(e.Loc)
-}
 
 func main()  {
-	bytes:= washPostXML
 	var s Sitemapindex
+	var n News
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
 	xml.Unmarshal(bytes, &s)
-	fmt.Println(s.Locations)
+	news_map := make(map[string]NewsMap)
+
+	for _, Location := range s.Locations{
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+
+		for idx, _:= range n.Keywords{
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
 	}
+
+	for idx, data := range news_map{
+		fmt.Println("\n\n\n\n\n", idx)
+		fmt.Println("\n", data.Keyword)
+		fmt.Println("\n", data.Location)
+	}
+
+}
