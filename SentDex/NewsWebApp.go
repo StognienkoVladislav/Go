ac4cb05f8f71ae998be151f4aedb6f1c6441def6
Vladislav Stognienko <stognienkovv@gmail.com>
2018-03-13 11:39:18 +0200

    some trash with goroutine

--- SentDex/NewsWebApp.go
+++ SentDex/NewsWebApp.go
@@ -6,7 +6,10 @@ import (
 	"html/template"
 	"encoding/xml"
 	"io/ioutil"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 type NewsMap struct {
 	Keyword string
@@ -32,26 +35,42 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
 }
 
-func newsAggHandler(w http.ResponseWriter, r *http.Request) {
-	var s Sitemapindex
+func newsRoutine(c chan News, Location string){
+	defer wg.Done()
 	var n News
+	resp, _ := http.Get(Location)
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &n)
+	resp.Body.Close()
+	c <- n
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+
+	var s Sitemapindex
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	xml.Unmarshal(bytes, &s)
 	news_map := make(map[string]NewsMap)
+	resp.Body.Close()
+	queue := make(chan News, 30)
 
 	for _, Location := range s.Locations {
-		resp, _ := http.Get(Location)
-		bytes, _ := ioutil.ReadAll(resp.Body)
-		xml.Unmarshal(bytes, &n)
+		wg.Add(1)
+		go newsRoutine(queue, Location)
+	}
+	wg.Wait()
+	close(queue)
 
-		for idx, _ := range n.Keywords {
-			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+	for elem := range queue {
+		for idx, _ := range elem.Keywords {
+			news_map[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
 		}
 	}
 
 	p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
-	t, _ := template.ParseFiles("newsaggtemplate.html")
+
+	t, _ := template.ParseFiles("aggregatorfinish.html")
 	t.Execute(w, p)
 }
 
